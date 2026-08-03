1250cfc971549508f27f58cf6ce53292d8096fbb
Ashiq A <292994832+ashiq-webdev@users.noreply.github.com>
2026-08-03 08:48:53 +0530

    refactor: restructure routes for single-page scroll with project detail pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import { Routes, Route } from 'react-router';
 import { Layout } from './components/Layout/Layout';
 import { HomePage } from './pages/HomePage';
-import { ProjectsPage } from './pages/ProjectsPage';
-import { AboutPage } from './pages/AboutPage';
-import { ContactPage } from './pages/ContactPage';
 import { ProjectDetailPage } from './pages/ProjectDetailPage';
 import { NotFoundPage } from './pages/NotFoundPage';
-import { useTheme } from './utils/useTheme';
 
 function App() {
-  const { theme, toggleTheme } = useTheme();
-
   return (
     <Routes>
-      <Route element={<Layout theme={theme} toggleTheme={toggleTheme} />}>
-        <Route index element={<HomePage theme={theme} />} />
-        <Route path="projects" element={<ProjectsPage theme={theme} />} />
-        <Route path="projects/:slug" element={<ProjectDetailPage theme={theme} />} />
-        <Route path="about" element={<AboutPage theme={theme} />} />
-        <Route path="contact" element={<ContactPage theme={theme} />} />
-        <Route path="*" element={<NotFoundPage theme={theme} />} />
+      <Route element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="projects/:slug" element={<ProjectDetailPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
