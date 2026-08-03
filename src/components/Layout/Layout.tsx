86d9aab3c900fa028bb64f2fa4707b8d826e4e74
Ashiq A <292994832+ashiq-webdev@users.noreply.github.com>
2026-08-03 08:43:48 +0530

    refactor: remove theme props and max-width constraint from layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,11 @@
 import { Outlet } from 'react-router';
 import { Sidebar } from './Sidebar';
 
-type LayoutProps = {
-  theme: string;
-  toggleTheme: () => void;
-};
-
-export function Layout({ theme, toggleTheme }: LayoutProps) {
+export function Layout() {
   return (
     <div className="flex min-h-dvh" data-testid="layout">
-      <Sidebar theme={theme} onToggleTheme={toggleTheme} />
-      <main className="flex-1 px-6 py-10 md:px-24 md:py-20 max-w-5xl">
+      <Sidebar />
+      <main className="flex-1 px-6 py-10 md:px-24 md:py-20">
         <Outlet />
       </main>
     </div>
